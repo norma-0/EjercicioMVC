81e9b3410b644732278c6f029e32b61683b55bb9
Ariel Ascencio <isaiascencio45@outlook.com>
2026-04-27 17:54:09 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EjercicioMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
